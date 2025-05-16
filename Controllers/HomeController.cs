d3c0c4dc8bcaef79cd969d780b6f184c96820954
Ederson E Junior <ed.dev46@gmail.com>
2025-05-16 12:29:56 -0300

    Initial Setup

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntegrationRickAndMortyAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Dictionary<int, string> characters = new Dictionary<int, string>
+            {
+                { 1, "Rick Sanchez" },
+                { 2, "Morty Smith" },
+                { 3, "Summer Smith" },
+                { 4, "Beth Smith" },
+                { 5, "Jerry Smith" }
+            };
+            return View(characters);
+        }
+    }
+}
